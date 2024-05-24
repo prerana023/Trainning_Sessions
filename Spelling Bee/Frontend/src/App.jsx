617da9b0185575b0583f7a520011bad0fcbb29d2
Ramailo Prerana <prerana@ramailo.tech>
2024-05-24 13:47:10 +0545

    Create containers and layout of game

--- Spelling Bee/Frontend/src/App.jsx
+++ Spelling Bee/Frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  const [currentDate, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    const now = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = now.toLocaleDateString('en-US', options);
+    setCurrentDate(formattedDate);
+  }, []);
   return (
     <>
-    <div className=' flex flex-col items-center'>
-      <ul className=''>
-      </ul>
+    <div className='flex border-y-2'>
+      {/* title */}
+      <div className='m-5 p-6'>
+        <span className='font-mono font-bold text-3xl mx-4'>Spelling Bee</span>
+        <span className='font-mono text-2xl'>{currentDate}</span>
+      </div>
+
+      {/* nav bar */}
+      <div>
+      </div>
+
+      {/* game UI */}
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+
     </div>
     </>
   )
